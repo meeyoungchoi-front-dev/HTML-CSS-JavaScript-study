40ccba46efaca112722373da7f1a717192eed5b0
choimeeyoung <choimeeyoung94@gmail.com>
2024-03-31 16:34:38 +0900

    불필요한 alert 제거

--- cyworld/script/script.js
+++ cyworld/script/script.js
@@ -6,17 +6,14 @@ window.addEventListener('load', function() {
     homeButton.addEventListener("click", homeButtonClick);
 
     function homeButtonClick() {
-        alert("클릭");
         document.getElementById("contentFrame").setAttribute("src", "home.html");
     }
 
     gameButton.addEventListener('click', function(event) {
-        alert("클릭");
         document.getElementById("contentFrame").setAttribute("src", "game.html");
     });
 
     musicButton.addEventListener('click', function(event) {
-        alert("클릭");
         document.getElementById("contentFrame").setAttribute("src", "zukbox.html");
     });
 });
