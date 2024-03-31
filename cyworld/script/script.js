310708be169a66a59c9f693ab13028989ca5dd4e
choimeeyoung <choimeeyoung94@gmail.com>
2024-03-31 15:59:37 +0900

    update source code : 윈도우 로드 이후에 addEventListener load 될 수 있도록 수정

--- cyworld/script/script.js
+++ cyworld/script/script.js
@@ -1,29 +1,22 @@
-let homeButton = document.getElementById('home');
-let gameButton = document.getElementById('game');
-let musicButton = document.getElementById('music');
+window.addEventListener('load', function() {
+    let homeButton = document.getElementById('home');
+    let gameButton = document.getElementById('game');
+    let musicButton = document.getElementById('music');
 
+    homeButton.addEventListener("click", homeButtonClick);
 
-homeButton.addEventListener("click", homeButtonClick);
+    function homeButtonClick() {
+        alert("클릭");
+        document.getElementById("contentFrame").setAttribute("src", "home.html");
+    }
 
-function homeButtonClick() {
-    alert("클릭");
-    document.getElementById("contentFrame").setAttribute("src", "home.html");
-}
+    gameButton.addEventListener('click', function(event) {
+        alert("클릭");
+        document.getElementById("contentFrame").setAttribute("src", "game.html");
+    });
 
-gameButton.addEventListener('click', function(event) {
-    alert("클릭");
-    document.getElementById("contentFrame").setAttribute("src", "game.html");
+    musicButton.addEventListener('click', function(event) {
+        alert("클릭");
+        document.getElementById("contentFrame").setAttribute("src", "zukbox.html");
+    });
 });
-
-musicButton.addEventListener('click', function(event) {
-    alert("클릭");
-    document.getElementById("contentFrame").setAttribute("src", "zukbox.html");
-});
-
-
-
-
-
-
-
-
