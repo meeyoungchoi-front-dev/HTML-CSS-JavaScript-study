b0fccbdb685337e90d42a1224ff2dbcf6a05b3f1
choimeeyoung <choimeeyoung94@gmail.com>
2024-04-03 12:33:13 +0900

    조건문 switch - 홀수 짝수 판별하기

--- class/03-javascript/chapter02.js
+++ class/03-javascript/chapter02.js
@@ -10,3 +10,33 @@ if (a > b) {
 } else {
     console.log('b가 더 크고 b와 c가 다릅니다!');
 }
+
+// switch
+const number = 10;
+
+switch(number) {
+    case 1:
+        console.log(number);
+        break;
+    case 10:
+        console.log(number);
+        break;
+        default:
+            console.log('아무것도 해당되지 않아요!');
+}
+
+// if 와 switch 사용해서 짝수 홀수 구분하기
+if (number % 2 == 0) {
+    console.log('짝수!');
+} else {
+    console.log('홀수!');
+}
+
+switch  (number % 2 ) {
+    case 0:
+        console.log('짝수');
+        break;
+    case 1:
+        console.log('홀수');
+        break;
+}
