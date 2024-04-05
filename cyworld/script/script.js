0c649e2c16bb1ba5d3fad19f03ae21fab39e5b5d
choimeeyoung <choimeeyoung94@gmail.com>
2024-04-05 19:00:50 +0900

    싸이월드 버튼 클릭시 색변경하는 코드 리팩토링

--- cyworld/script/script.js
+++ cyworld/script/script.js
@@ -1,25 +1,13 @@
 window.addEventListener('load', function() {
-    const homeButton = document.getElementById('home');
-    const gameButton = document.getElementById('game');
-    const musicButton = document.getElementById('music');
-
-    homeButton.addEventListener("click", homeButtonClick);
-
-    function homeButtonClick(event) {
-        document.getElementById("contentFrame").setAttribute("src", "home.html");
-        homeButton.style.backgroundColor = "gray";
-        homeButton.style.color = "black";
+    const buttons = document.querySelectorAll(".side__wrapper__button");
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function(event) {
+            if (event.currentTarget === buttons[i]) {
+                buttons[i].classList.add("active");
+            } 
+        })
     }
-
-    gameButton.addEventListener('click', function(event) {
-        document.getElementById("contentFrame").setAttribute("src", "game.html");
-        gameButton.style.backgroundColor = "gray";
-        gameButton.style.color = "black";
-    });
-
-    musicButton.addEventListener('click', function(event) {
-        document.getElementById("contentFrame").setAttribute("src", "zukbox.html");
-        musicButton.style.backgroundColor = "gray";
-        musicButton.style.color = "black";
-    });
 });
+
+
