36d7c5faf6de2751d43e09a0bdeba5b9a40ece5d
choimeeyoung <choimeeyoung94@gmail.com>
2024-04-05 21:04:22 +0900

    javascript - 이벤트를 등록하고 삭제하는 방법

---
+++ 03-javascript/chapter07.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const removeButton = document.querySelector('.remove');
+
+function handler(event) {
+    console.log('😍😍😍😍😍😍😍');
+}
+
+function removeHandler(event) {
+    button.removeEventListener('click', handler);
+}
+
+button.addEventListener('click', handler);
+removeButton.addEventListener('click', removeHandler);
